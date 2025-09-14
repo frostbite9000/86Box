3bf55f2f600c3850c1e22d5c8a785b254a86e216
aqua-rgb <kramosstellitanolira@gmail.com>
2025-09-13 21:52:40 -0700

    fix elsewhere

--- src/cpu/386_dynarec.c
+++ src/cpu/386_dynarec.c
@@ -314,8 +314,6 @@ exec386_dynarec_int(void)
             cpu_state.eflags &= ~(RF_FLAG);
 #    endif
             x86_opcodes[(opcode | cpu_state.op32) & 0x3ff](fetchdat);
-
-            sse_xmm = 0;
         }
 
 #    ifndef USE_NEW_DYNAREC
@@ -566,7 +564,6 @@ exec386_dynarec_dyn(void)
                 codegen_generate_call(opcode, x86_opcodes[(opcode | cpu_state.op32) & 0x3ff], fetchdat, cpu_state.pc, cpu_state.pc - 1);
 
                 x86_opcodes[(opcode | cpu_state.op32) & 0x3ff](fetchdat);
-                sse_xmm = 0;
 
                 if (x86_was_reset)
                     break;
@@ -669,8 +666,6 @@ exec386_dynarec_dyn(void)
                 cpu_state.pc++;
 
                 x86_opcodes[(opcode | cpu_state.op32) & 0x3ff](fetchdat);
-
-                sse_xmm = 0;
 
                 if (x86_was_reset)
                     break;
@@ -942,9 +937,6 @@ exec386(int32_t cycs)
                 cpu_state.eflags &= ~(RF_FLAG);
 #endif
                 x86_opcodes[(opcode | cpu_state.op32) & 0x3ff](fetchdat);
-
-                sse_xmm = 0;
-
                 if (x86_was_reset)
                     break;
             }
